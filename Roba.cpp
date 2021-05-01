2317636484e48569d2697b45d7382287e7807072
Trenord <54724171+Trenord@users.noreply.github.com>
2021-05-01 12:00:23 +0200

    Update Roba.cpp

--- Roba.cpp
+++ Roba.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main (){
   //ordinamento
   for (int i = 0; i < n; i++) {
-    for (j = i + 1; j < n; j++)
+    for (int j = i + 1; j < n; j++)
     {
       if (a[j] < a[i]) {
         temp = a[i];

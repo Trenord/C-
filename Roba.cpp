6c5b8141348fb8bd12492373510bc12b0a1148c0
Trenord <54724171+Trenord@users.noreply.github.com>
2021-05-01 11:58:30 +0200

    Update Roba.cpp

--- Roba.cpp
+++ Roba.cpp
@@ -4,16 +4,14 @@ using namespace std;
 
 int main (){
   //ordinamento
-  	for (int i = 0; i < n; i++) {
-        for (j = i + 1; j < n; j++)
-        {
-            if (a[j] < a[i]) {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }
+  for (int i = 0; i < n; i++) {
+    for (j = i + 1; j < n; j++)
+    {
+      if (a[j] < a[i]) {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
     }
-  
-  
+  }
 }
